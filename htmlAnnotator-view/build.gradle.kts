d33d6f273292dd2b1f985d1a5ad08acc6c5a469f
RavenLiao <ravenliao@163.com>
2024-04-21 18:17:49 +0800

    feat: add htmlAnnotator-view module

---
+++ htmlAnnotator-view/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinAndroid)
+}
+
+android {
+    namespace = "com.ravenl.htmlannotator.view"
+    compileSdk = property("compileSdk").toString().toInt()
+
+    defaultConfig {
+        minSdk = property("minSdk").toString().toInt()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    api(project(":htmlAnnotator-core"))
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.bundles.android.test)
+}
