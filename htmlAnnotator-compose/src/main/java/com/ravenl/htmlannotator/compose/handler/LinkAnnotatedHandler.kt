f4ce76ce018720310a2829f55ab25d5d517847a0
RavenLiao <ravenliao@163.com>
2024-05-06 20:34:44 +0800

    feat: make LinkHandler open

--- htmlAnnotator-compose/src/main/java/com/ravenl/htmlannotator/compose/handler/LinkAnnotatedHandler.kt
+++ htmlAnnotator-compose/src/main/java/com/ravenl/htmlannotator/compose/handler/LinkAnnotatedHandler.kt
@@ -4,7 +4,7 @@ import com.ravenl.htmlannotator.compose.styler.LinkAnnotatedStyler
 import com.ravenl.htmlannotator.core.TextStyler
 import com.ravenl.htmlannotator.core.handler.AbsLinkHandler
 
-class LinkAnnotatedHandler : AbsLinkHandler() {
+open class LinkAnnotatedHandler : AbsLinkHandler() {
     override fun getUrlStyler(url: String, start: Int, end: Int): TextStyler =
         LinkAnnotatedStyler(url, start, end)
 }
--- htmlAnnotator-view/src/main/java/com/ravenl/htmlannotator/view/handler/LinkSpannedHandler.kt
+++ htmlAnnotator-view/src/main/java/com/ravenl/htmlannotator/view/handler/LinkSpannedHandler.kt
@@ -5,7 +5,7 @@ import com.ravenl.htmlannotator.core.TextStyler
 import com.ravenl.htmlannotator.core.handler.AbsLinkHandler
 import com.ravenl.htmlannotator.view.styler.SpannedStyler
 
-class LinkSpannedHandler : AbsLinkHandler() {
+open class LinkSpannedHandler : AbsLinkHandler() {
     override fun getUrlStyler(url: String, start: Int, end: Int): TextStyler =
         SpannedStyler(start, end, URLSpan(url))
 }
